d21cc60d9839342a88c8ab465d4a168cd9a5595d
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-12-05 09:55:49 +0100

    in h

--- ProofLite/mumukk/mumukk.h
+++ ProofLite/mumukk/mumukk.h
@@ -1556,6 +1556,26 @@ void mumukk::Init(TTree *tree)
    fChain->SetBranchAddress("kaon2_Meas_byHits", &kaon2_Meas_byHits, &b_kaon2_Meas_byHits);
 
 
+     outTree = new TTree("outTree","outTree");
+
+     //cw values
+    X_mass = 0;
+    kk_mass = 0;
+    mumu_mass = 0;
+    X_LFly = 0;
+    X_pt = 0;
+    X_eta = 0;
+    X_vtx = 0;
+    X_cosAlpha = 0;
+
+        outTree->SetBranchAddress("X_mass_out", &X_mass, &X_mass_b);
+        outTree->SetBranchAddress("kk_mass_out", &kk_mass, &kk_mass_b);
+        outTree->SetBranchAddress("mumu_mass_out", &mumu_mass, &mumu_mass_b);
+        outTree->SetBranchAddress("X_LFly_out", &X_LFly, &X_LFly_b);
+        outTree->SetBranchAddress("X_eta_out", &X_eta, &X_eta_b);
+        outTree->SetBranchAddress("X_vtx_out", &X_vtx, &X_vtx_b);
+        outTree->SetBranchAddress("X_cosAlpha_out", &X_cosAlpha, &X_cosAlpha_b);
+        
 
 
 }
