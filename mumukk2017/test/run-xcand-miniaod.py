6c48aaa645e34e2ba50e9446227b1a671f586bd6
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-12-13 19:52:01 +0100

    Onia2MuMuFilteredPhi

--- mumukk2017/test/run-xcand-miniaod.py
+++ mumukk2017/test/run-xcand-miniaod.py
@@ -78,17 +78,17 @@ process.Onia2MuMuFilteredPhi = cms.EDProducer('DiMuonFilter',
 
 )
 
-process.DiMuonCounterJPsi = cms.EDFilter('CandViewCountFilter',
-    src       = cms.InputTag("Onia2MuMuFilteredJpsi"),
-    minNumber = cms.uint32(1),
-    filter    = cms.bool(True)
-)
-
-process.DiMuonCounterPhi = cms.EDFilter('CandViewCountFilter',
-    src       = cms.InputTag("Onia2MuMuFilteredPhi"),
-    minNumber = cms.uint32(1),
-    filter    = cms.bool(True)
-)
+# process.DiMuonCounterJPsi = cms.EDFilter('CandViewCountFilter',
+#     src       = cms.InputTag("Onia2MuMuFilteredJpsi"),
+#     minNumber = cms.uint32(1),
+#     filter    = cms.bool(True)
+# )
+#
+# process.DiMuonCounterPhi = cms.EDFilter('CandViewCountFilter',
+#     src       = cms.InputTag("Onia2MuMuFilteredPhi"),
+#     minNumber = cms.uint32(1),
+#     filter    = cms.bool(True)
+# )
 
 process.xProducer = cms.EDProducer('FourOniaProducer',
     phidimuons          = cms.InputTag("Onia2MuMuFiltered"),
