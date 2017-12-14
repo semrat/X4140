96b8dd8205caf8d159709d6973108025ee305e17
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-12-14 14:28:50 +0100

    hltDiMuonGlbOrTrk0zFiltered0p2v2

--- mumukk2017/test/run-xcand-miniaod.py
+++ mumukk2017/test/run-xcand-miniaod.py
@@ -64,17 +64,17 @@ process.FourOnia2MuMuJPsi = cms.EDProducer('FourOnia2MuMuPAT',
 process.Onia2MuMuFilteredJpsi = cms.EDProducer('DiMuonFilter',
       OniaTag             = cms.InputTag("FourOnia2MuMuJPsi"),
       singlemuonSelection = cms.string(""),
-      dimuonSelection     = cms.string("2.95 < mass && mass < 3.15 && pt > 10. && abs(y) < 2.5 && charge==0 && userFloat('vProb') > 0.01"),
+      dimuonSelection     = cms.string("2.95 < mass && mass < 3.15"),
       do_trigger_match    = cms.bool(True),
-      HLTFilters          = cms.vstring('hltMumuFilterDoubleMu2Jpsi'),
+      HLTFilters          = cms.vstring('hltDiMuonGlbOrTrk0zFiltered0p2v2'),
 )
 
 process.Onia2MuMuFilteredPhi = cms.EDProducer('DiMuonFilter',
       OniaTag             = cms.InputTag("FourOnia2MuMuPhi"),
       singlemuonSelection = cms.string(""),
-      dimuonSelection     = cms.string("0.92 < mass && mass < 1.12 && pt > 10. && abs(y) < 2.5 && charge==0 && userFloat('vProb') > 0.01"),
+      dimuonSelection     = cms.string("0.92 < mass && mass < 1.12"),
       do_trigger_match    = cms.bool(True),
-      HLTFilters          = cms.vstring('hltMumuFilterDoubleMu2Jpsi'),
+      HLTFilters          = cms.vstring('hltDiMuonGlbOrTrk0zFiltered0p2v2'),
 
 )
 
