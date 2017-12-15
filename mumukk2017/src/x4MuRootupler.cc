f5ba5aad1f297fcd49c9503bcb06c98227128ff1
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-12-15 19:17:21 +0100

    dynamic_cast<GlobalPoint>(

--- mumukk2017/src/x4MuRootupler.cc
+++ mumukk2017/src/x4MuRootupler.cc
@@ -87,9 +87,9 @@ class x4MuRootupler:public edm::EDAnalyzer {
 	TLorentzVector gen_photon_p4;
 	TLorentzVector gen_muonP_p4;
 	TLorentzVector gen_muonM_p4;
-  Point xVertex;
-  Point jpsVertex;
-  Point phiVertex;
+  GlobalPoint xVertex;
+  GlobalPoint jpsVertex;
+  GlobalPoint phiVertex;
 
   edm::EDGetTokenT<reco::GenParticleCollection> genCands_;
 
@@ -144,9 +144,9 @@ isMC_(iConfig.getParameter < bool > ("isMC"))
 
     x_tree->Branch("dz",           &dz,           "dz/D");
 
-    x_tree->Branch("xVertex",  "Point", &xVertex);
-    x_tree->Branch("jpsVertex",  "Point", &jpsVertex);
-    x_tree->Branch("phiVertex",  "Point", &phiVertex);
+    x_tree->Branch("xVertex",  "GlobalPoint", &xVertex);
+    x_tree->Branch("jpsVertex",  "GlobalPoint", &jpsVertex);
+    x_tree->Branch("phiVertex",  "GlobalPoint", &phiVertex);
 
     //
     // x_tree->Branch("rf1S_chi_p4", "TLorentzVector", &rf1S_chi_p4);
@@ -312,9 +312,9 @@ void x4MuRootupler::analyze(const edm::Event & iEvent, const edm::EventSetup & i
       for (unsigned int i=0; i< xcand_hand->size(); i++) {
         pat::CompositeCandidate x_ = xcand_hand->at(i);
 
-        xVertex  = x_.vertex();
-        phiVertex = x_.daughter("phi")->vertex();
-        jpsVertex = x_.daughter("jps")->vertex();
+        xVertex  = dynamic_cast<GlobalPoint>(x_.vertex());
+        phiVertex = dynamic_cast<GlobalPoint>(x_.daughter("phi")->vertex());
+        jpsVertex = dynamic_cast<GlobalPoint>(x_.daughter("jps")->vertex());
         dz = x_.userFloat("dz");
 
         std::cout<<"Cycling on ups"<<std::endl;
