739f48556495ed94697f4d9a9e4ac93674dacb7d
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-01-06 19:17:46 +0100

    reco::Vertex

--- mumukk2017/src/x4MuRootupler.cc
+++ mumukk2017/src/x4MuRootupler.cc
@@ -75,9 +75,9 @@ class x4MuRootupler:public edm::EDAnalyzer {
   Point xVertex;
   Point jpsVertex;
   Point phiVertex;
-  const reco::Vertex commonVertex;
+  reco::Vertex commonVertex;
   reco::Vertex PVwithmuons;
-  const reco::Vertex muLessVertex;
+  reco::Vertex muLessVertex;
 
   edm::EDGetTokenT<reco::GenParticleCollection> genCands_;
 
@@ -274,9 +274,9 @@ void x4MuRootupler::analyze(const edm::Event & iEvent, const edm::EventSetup & i
         phiVertex = x_.daughter("phi")->vertex();
         jpsVertex = x_.daughter("jpsi")->vertex();
         std::cout<<"debug :" << debug <<std::endl; debug++;
-        PVwithmuons = (x_.userData<reco::Vertex>("PVwithmuons")).get();
-        muLessVertex = *(x_.userData<reco::Vertex>("muonlessPV"));
-        commonVertex = *(x_.userData<reco::Vertex>("commonVertex"));
+        PVwithmuons = const_cast<reco::Vertex>(x_.userData<reco::Vertex>("PVwithmuons"));
+        muLessVertex = (x_.userData<reco::Vertex>("muonlessPV"));
+        commonVertex = (x_.userData<reco::Vertex>("commonVertex"));
         std::cout<<"debug :" << debug <<std::endl; debug++;
         countTksOfPV = x_.userInt("countTksOfPV");
         vertexWeight = x_.userFloat("vertexWeight");
