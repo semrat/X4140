3c41bc1e619a27c3db65c9eca6be2024fd4b3138
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-01-06 17:27:18 +0100

    PVwithmuons

--- mumukk2017/src/x4MuRootupler.cc
+++ mumukk2017/src/x4MuRootupler.cc
@@ -274,9 +274,9 @@ void x4MuRootupler::analyze(const edm::Event & iEvent, const edm::EventSetup & i
         phiVertex = x_.daughter("phi")->vertex();
         jpsVertex = x_.daughter("jpsi")->vertex();
         std::cout<<"debug :" << debug <<std::endl; debug++;
-        PVwithmuons = *(x_.userData<Point>("PVwithmuons"));
-        muLessVertex = *(x_.userData<Point>("muonlessPV"));
-        commonVertex = *(x_.userData<Point>("commonVertex"));
+        PVwithmuons = (x_.userData<Point>("PVwithmuons"));
+        muLessVertex = (x_.userData<Point>("muonlessPV"));
+        commonVertex = (x_.userData<Point>("commonVertex"));
         std::cout<<"debug :" << debug <<std::endl; debug++;
         countTksOfPV = x_.userInt("countTksOfPV");
         vertexWeight = x_.userFloat("vertexWeight");
