c2b38663f8448be7283cd69d6eb2dc3fea2d0e4c
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-01-15 17:44:40 +0100

    filters

--- jpsiphi2017/src/FourOnia2MuMu.cc
+++ jpsiphi2017/src/FourOnia2MuMu.cc
@@ -65,12 +65,21 @@ UInt_t FourOnia2MuMuPAT::isTriggerMatched(pat::CompositeCandidate *diMuon_cand) 
 
   // if matched a given trigger, set the bit, in the same order as listed
   for (unsigned int iTr = 0; iTr<HLTFilters_.size(); iTr++ ) {
-    std::cout << HLTFilters_[iTr] << std::endl;
+    // std::cout << HLTFilters_[iTr] << std::endl;
     const pat::TriggerObjectStandAloneCollection mu1HLTMatches = muon1->triggerObjectMatchesByFilter(HLTFilters_[iTr]);
     const pat::TriggerObjectStandAloneCollection mu2HLTMatches = muon2->triggerObjectMatchesByFilter(HLTFilters_[iTr]);
     if (!mu1HLTMatches.empty() && !mu2HLTMatches.empty()) matched += (1<<iTr);
   }
+  const pat::TriggerObjectStandAloneCollection muon1Collection = muon1->triggerObjectMatches();
+  const pat::TriggerObjectStandAloneCollection muon2Collection = muon2->triggerObjectMatches();
+
+  for ( size_t i = 0; i < muon1Collection.size(); ++i )
+    for ( size_t j = 0; j < muon1->triggerObjectMatch(i)->filterLabels().size(); ++j )
+      std::cout << (muon1->triggerObjectMatch(i)->filterLabels())[j] << std::endl;
+
   std::cout << "Triggers matched : " << matched << std::endl;
+  std::cout << "Sizes : " << muon1Collection.size() << " - " << muon2Collection.size() << std::endl;
+
   return matched;
 }
 
