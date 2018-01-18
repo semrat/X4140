d765cf607a130e594b475f4628a253973e8d34a9
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-01-18 19:54:10 +0100

    hltDoubleMu2JpsiDoubleTrkL3Filtered

---
+++ jpsiphi2017/test/crab_4muons.py
@@ -0,0 +1,83 @@
+import sys
+import os
+
+jsonFile="Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON_MuonPhys.txt"
+run = "F"
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+#print("Test = " + str(skipevt))
+
+jobdir = 'X4140_' + run
+
+if not os.path.exists(jobdir):
+    os.makedirs(jobdir)
+
+datasetnames = {
+"F" : '/MuOnia/Run2017F-PromptReco-v1/MINIAOD',
+}
+
+runNumber = [
+#'274094-274240',
+'',
+'305388-307000',
+'305388-306000',
+'305388-305700',
+'305700-306000',
+'306000-306100',
+'306100-306200',
+'306200-306300',
+'306300-306500',
+#'273158',
+]
+
+
+datasetName = datasetnames[run]
+runNum = runNumber[0]
+#lumi = jsonfile[jNum]
+lumi = jsonFile
+#HLT = HLTPath[0]
+
+import datetime
+timestamp = datetime.datetime.now().strftime("_%Y%m%d_%H%M%S")
+
+dataset = filter(None, datasetName.split('/'))
+
+config.section_('General')
+config.General.transferOutputs  = True
+config.General.workArea         = jobdir
+#config.General.requestName     = 'JetHT_Run2015D_PromptReco_v4_RECO'+timestamp
+#config.General.requestName             = dataset[0]+'_'+dataset[1]+'_'+dataset[2]+'_'+runNum+'_'+HLT+timestamp
+config.General.requestName      = 'fourMuons_phiJpsiTriggers_' + dataset[0]+'_'+dataset[1]+'_'+dataset[2]+'_'+runNum+'_'+timestamp #+'_split_'+ jsonFile.split('_')[-1].split('.')[0]
+config.General.transferLogs     = False
+
+config.section_('JobType')
+config.JobType.psetName         = '/lustre/home/adrianodif/CMSSW_9_2_2/src/mmkk/mmkk/test/run-4mu-miniaod.py'
+config.JobType.pluginName       = 'Analysis'
+config.JobType.maxMemoryMB      = 2500
+config.JobType.maxJobRuntimeMin = 2750
+config.JobType.allowUndistributedCMSSW = True
+#config.JobType.inputFiles      = ['Run_time.txt']
+#config.JobType.outputFiles     = ['EventList.txt','EventListClean.txt']
+
+config.section_('Data')
+config.Data.inputDataset        = datasetName
+config.Data.inputDBS            = 'global'
+config.Data.totalUnits          = -1
+config.Data.unitsPerJob         = 1
+config.Data.splitting           = 'LumiBased'
+config.Data.runRange            = runNum
+config.Data.lumiMask            = lumi
+config.Data.outLFNDirBase       = '/store/user/adiflori/'
+config.Data.publication         = False
+config.Data.ignoreLocality      = True
+
+
+config.section_('Site')
+#config.Site.storageSite        = 'T2_CH_CERN'
+config.Site.storageSite         = 'T2_IT_Bari'
+#config.Site.blacklist          = ['T2_IN_TIFR','T2_US_Vanderbilt']
+config.Site.blacklist           = ['T1*']
+#config.Site.whitelist           = ['T2_IT_Legnaro']
+#config.Site.whitelist          = ['T2_IT_Bari','T2_IT_Pisa','T2_IT_Rome','T2_IT_Legnaro']
--- jpsiphi2017/test/run-4mu-miniaod.py
+++ jpsiphi2017/test/run-4mu-miniaod.py
@@ -22,47 +22,96 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
 
 process.load("mmkk.mmkk.slimmedMuonsTriggerMatcher2017_cfi")
 
-hltpaths = cms.vstring('HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi',
-                        'HLT_Mu20_TkMu0_Phi',
-                        'HLT_Dimuon14_Phi_Barrel_Seagulls',
-                        'HLT_Mu25_TkMu0_Phi',
-                        'HLT_Dimuon24_Phi_noCorrL1'
-                        )
+hltList =
+[
+#Phi
+'HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi',
+'HLT_DoubleMu2_Jpsi_DoubleTrk1_Phi',
+'HLT_Mu20_TkMu0_Phi',
+'HLT_Dimuon14_Phi_Barrel_Seagulls',
+'HLT_Mu25_TkMu0_Phi',
+'HLT_Dimuon24_Phi_noCorrL1',
+#JPsi
+'HLT_DoubleMu4_JpsiTrkTrk_Displaced',
+'HLT_DoubleMu4_JpsiTrk_Displaced',
+'HLT_DoubleMu4_Jpsi_Displaced',
+'HLT_DoubleMu4_3_Jpsi_Displaced',
+'HLT_Dimuon20_Jpsi_Barrel_Seagulls',
+'HLT_Dimuon25_Jpsi',
+'HLT_Dimuon0_Jpsi',
+]
 
-hltpathsV = cms.vstring('HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi', 'HLT_Mu20_TkMu0_Phi',
-                        'HLT_Dimuon14_Phi_Barrel_Seagulls','HLT_Mu25_TkMu0_Phi',
-                        'HLT_Dimuon24_Phi_noCorrL1')
+hltpaths = cms.vstring(hltList)
 
-filters = cms.vstring(          #HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi
+hltpathsV = cms.vstring([h + '_v*' for h in hltList])
+
+filters = cms.vstring(
+                                #PHI TRIGGERS FILTER
+                                #HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi
                                 'hltDiMuonGlbOrTrkFiltered0v2', #Phi
                                 'hltDiMuonGlbOrTrk0zFiltered0p2v2',
                                 'hltDoubleMu2JpsiL3Filtered', ##JPsi
                                 'hltMumuVtxProducerDoubleMu2Jpsi',
                                 'hltMumuFilterDoubleMu2Jpsi',
+                                #HLT_DoubleMu2_Jpsi_DoubleTrk1_Phi
+                                'hltDoubleMu2JpsiDoubleTrkL3Filtered',
+                                'hltDoubleTrkmumuVtxProducerDoubleMu2Jpsi',
+                                'hltDoubleTrkmumuFilterDoubleMu2Jpsi',
+                                'hltJpsiTkAllConeTracksIterDoubleTrk',
+                                'hltJpsiTrkTrkVertexProducerPhiDoubleTrk1v2',
+                                'hltJpsiTkTkVertexFilterPhiDoubleTrk1v2',
+                                #HLT_Mu20_TkMu0_Phi
+                                'hltL3fL1sMu16orMu18erorMu20L1f0L2f0L3Filtered20',
+                                'hltDiMuonGlbFiltered20TrkFiltered0',
+                                'hltDiMuonGlb20Trk0DzFiltered0p2',
                                 #HLT_Dimuon14_Phi_Barrel_Seagulls
                                 'hltDimuon14PhiBarrelnoCowL3Filtered',
                                 'hltDisplacedmumuVtxProducerDimuon14PhiBarrelnoCow',
                                 'hltDisplacedmumuFilterDimuon14PhiBarrelnoCow',
                                 #HLT_Mu25_TkMu0_Phi
-                                'hltDimuon14PhiBarrelnoCowL3Filtered',
-                                'hltDisplacedmumuVtxProducerDimuon14PhiBarrelnoCow',
-                                'hltDisplacedmumuFilterDimuon14PhiBarrelnoCow',
-                                #HLT_Mu20_TkMu0_Phi
                                 'hltL3fL1sMu16orMu18erorMu20L1f0L2f0L3Filtered20',
-                                'hltDiMuonGlbFiltered20TrkFiltered0',
-                                'hltDiMuonGlb20Trk0DzFiltered0p2',
+                                'hltDiMuonGlbFiltered25TrkFiltered0',
+                                'hltDiMuonGlb25Trk0DzFiltered0p2',
+                                #HLT_Dimuon24_Phi_noCorrL1
+                                'hltDisplacedmumuFilterDimuon24PhiBarrelNoCorrL1',
+                                'hltDisplacedmumuVtxProducerDimuon24PhiNoCorrL1',
+                                'hltDimuon24PhiNoCorrL1L3fL3Filtered',
+                                #JPSI Trigger Filters
+                                #HLT_DoubleMu4_JpsiTrkTrk_Displaced_v4
+                                'hltDoubleMu4JpsiDisplacedL3Filtered'
+                                'hltDisplacedmumuVtxProducerDoubleMu4Jpsi',
+                                'hltDisplacedmumuFilterDoubleMu4Jpsi',
+                                'hltJpsiTkAllConeTracksIter',
+                                'hltJpsiTrkTrkVertexProducerPhiKstar',
+                                'hltJpsiTkTkVertexFilterPhiKstar',
+                                #HLT_DoubleMu4_JpsiTrk_Displaced_v12
+                                'hltDoubleMu4JpsiDisplacedL3Filtered',
+                                'hltDisplacedmumuFilterDoubleMu4Jpsi',
+                                'hltJpsiTkVertexProducer',
+                                'hltJpsiTkVertexFilter',
+                                #HLT_DoubleMu4_Jpsi_Displaced
+                                'hltDoubleMu4JpsiDisplacedL3Filtered',
+                                'hltDisplacedmumuVtxProducerDoubleMu4Jpsi',
+                                'hltDisplacedmumuFilterDoubleMu4Jpsi',
+                                #HLT_DoubleMu4_3_Jpsi_Displaced
+                                'hltDoubleMu43JpsiDisplacedL3Filtered',
+                                'hltDisplacedmumuVtxProducerDoubleMu43Jpsi',
+                                'hltDisplacedmumuFilterDoubleMu43Jpsi',
+                                #HLT_Dimuon20_Jpsi_Barrel_Seagulls
+                                'hltDimuon20JpsiBarrelnoCowL3Filtered',
+                                'hltDisplacedmumuVtxProducerDimuon20JpsiBarrelnoCow',
+                                'hltDisplacedmumuFilterDimuon20JpsiBarrelnoCow',
+                                #HLT_Dimuon25_Jpsi
 
-)
+                                #HLT_Dimuon0_Jpsi
+                                'hltDimuon0JpsiL3Filtered',
+                                'hltDisplacedmumuVtxProducerDimuon0Jpsi',
+                                'hltDisplacedmumuFilterDimuon0Jpsi'
+                                )
 
 
 process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
-                                        triggerConditions = cms.vstring(
-                                                                        'HLT_DoubleMu2_Jpsi_DoubleTkMu0_Phi_v*',
-                                                                        'HLT_Mu20_TkMu0_Phi_v*',
-                                                                        'HLT_Dimuon14_Phi_Barrel_Seagulls_v*',
-                                                                        'HLT_Mu25_TkMu0_Phi_v*',
-                                                                        'HLT_Dimuon24_Phi_noCorrL1_v*'
-                                                                       ),
+                                        triggerConditions = cms.vstring(hltpathsV),
                                         hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
                                         l1tResults = cms.InputTag( "" ),
                                         throw = cms.bool(False)
